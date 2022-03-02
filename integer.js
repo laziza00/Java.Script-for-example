a78613cd431a94197d28b2371444447902d22d28
laziza@mail.ru <laziza00>
2022-03-02 10:10:08 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,207 @@
+
+
+// integer1
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+// let s = Math.sqrt(Math.sqrt((a**3 -b**3) + (a**3 + b**3)));
+// console.log(s);
+
+// integer2
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+// let s = (((1/6) * Math.sqrt(a)) +((1/3) * Math.sqrt(b) ));
+// s = s**2;
+// console.log(s);
+
+// integer3
+
+//  let a = +prompt('uch xonli son kiriting: ');
+//  let b = Math.floor(a /100);
+//  let c = Math.floor((a /10)%10);
+//  let d = Math.floor(a%10);
+//  let e= (d*100) + (c*10) + b;
+//  console.log(e);
+
+// integer4
+
+//  let a = +prompt('uch xonli son kiriting: ');
+//  let b = Math.floor(a /100);
+//  let c = Math.floor((a /10)%10);
+//  let d = Math.floor(a%10);
+//  let e=(c*100) + (b*10) +d;
+//  console.log(e);
+
+// integer5
+// let a = 6.3;
+// let b = 8.6;
+// let s = 9* (a ** 2*b) - 27 * (a ** 2* b ** 2) + 15 * (b ** 2);
+// console.log(s.toFixed(2));
+ 
+// integer6
+// let a= 3;
+// let b = 2;
+// let c = 4;
+// let d = 5;
+// let s = ((1+ (a/100))**b) / (Math.sqrt(c**2 + d**2));
+// console.log(s);
+
+// integer7
+// let a= -1.4;
+// let b = 7.9;
+// let c = 6.1;
+// let d = 9.9;
+// let s = Math.sqrt(((c-a)**2)+ ((d-b)**2 ))
+// console.log(s);
+
+// integer8
+// let a= -9.7;
+// let b = -9.9;
+// let c = 0.0;
+// let d = 2.9;
+// let e = -0.1;
+// let f = 5.2;
+
+
+// integer 9
+
+// let a = -0.5;
+// let x= Math.sin(a);
+// console.log(x);
+
+// integer 10
+
+// let a = 0.5;
+// let x= Math.cos(a);
+// console.log(x);
+
+
+// **************boolean*********************
+
+// integer 1 
+
+// let a= +prompt("a: ");
+// let b= +prompt("b: ");
+// console.log(a==b);
+
+// integer 2 
+// let a= +prompt("a: ");
+// let b = 7;
+// console.log(a==b);
+
+
+// integer 3 
+// let a= +prompt("a: ");
+// console.log(a>0? "True": "False");
+
+// integer 4 
+// let a= +prompt("a: ");
+// console.log(a<0? "True": "False");
+
+// integer 5
+// let  a = +prompt();
+// console.log(a%2==1? "True" : "False");
+
+// integer 6
+// let  a = +prompt();
+// console.log(a%2==0? "True" : "False");
+
+// integer 7
+
+// let a= +prompt("a: ");
+// let b = +prompt("b: ");
+// let c = +prompt("c: ");
+// console.log(b>a && b<c? "True": "False");
+
+// integer 8
+
+// let a= +prompt("a: ");
+// let b = +prompt("b: ");
+// let s= a + b;
+// console.log(s>0? "True": "False");
+
+// integer 9
+
+// let a= +prompt("a: ");
+// let b = +prompt("b: ");
+// let s= a + b;
+// console.log(s<0? "True": "False");
+
+// integer 10
+// let a= +prompt("a: ");
+// let b = +prompt("b: ");
+// let s= a + b;
+// console.log(s%2==0? "True": "False");
+
+
+
+
+// ****************integer 3 box******************
+// integer 11
+// let a= +prompt("a: ");
+// let b = (a/10) %10;
+// let s= a %10;
+// let d = b+ s;
+// let e = b*s
+// console.log(Math.floor(d),Math.floor(e));
+
+// integer 12
+// let a= +prompt();
+// let b = (a+3);
+// let c = (b**2)*2;
+// console.log(c);
+
+// integer 13
+// let a= +prompt();
+// let b = (3+a)/2;
+// let c = b**2;
+// console.log(c);
+
+// integer 14
+// let a= +prompt();
+// let b= +prompt();
+// let c = (a**b);
+// let d = (6**b);
+// let e = c + d;
+// console.log(e);
+
+// integer 15
+
+// let a= +prompt();
+// console.log(Math.abs(a));
+
+
+// integer 16
+
+// let a= +prompt();
+// console.log(a.toFixed(2));
+
+// integer 17
+
+// let a= +prompt();
+// let b= +prompt();
+// let c =Math.pow(a,4)+ 5*(Math.pow(a, 2)) + b*(Math.pow(a, 3));
+// console.log(c);
+
+// integer 18
+
+// let a=+prompt();
+// let b= +prompt();
+// let c =6*(Math.pow(a,3)* Math.pow(b,5))+ 4*(Math.pow(a, 4)* Math.pow(b, 3)) - 24*(a* b);
+// console.log(c);
+
+
+// integer 19 
+
+// let a = +prompt();
+//  let b = Math.floor(a /100);
+//  let c = Math.floor((a /10)%10);
+//  let d = Math.floor(a%10);
+//  let e= b+c+d;
+//  let f= b*c*d;
+//  console.log(e, f);
+
+// integer 20
+// let a = (1 + (2 / 3) - (3 + (1 / 4)));
+// console.log(Math.abs(a));
